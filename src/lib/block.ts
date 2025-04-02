67b899ac66f38498a5b8ddd850528c7409077438
Éderson Fernandes <efernandes.tech@gmail.com>
2025-04-01 23:40:50 -0300

    Add block hash

--- src/lib/block.ts
+++ src/lib/block.ts
@@ -1,18 +1,31 @@
+import sha256 from 'crypto-js/sha256';
+
 /**
  * Block class
  */
 export default class Block {
     index: number;
+    timestamp: number;
     hash: string;
+    previousHash: string;
+    data: string;
 
     /**
      * Creates a new block
      * @param index The block index in blockchain
-     * @param hash The block hash
+     * @param previousHash The previous block hash
+     * @param data The block data
      */
-    constructor(index: number, hash: string) {
+    constructor(index: number, previousHash: string, data: string) {
         this.index = index
-        this.hash = hash
+        this.timestamp = Date.now();
+        this.previousHash = previousHash;
+        this.data = data;
+        this.hash = this.getHash();
+    }
+
+    getHash(): string {
+        return sha256(this.index + this.data + this.timestamp + this.previousHash).toString();
     }
 
     /**
@@ -22,6 +35,9 @@ export default class Block {
     isValid(): boolean {
         if (this.index <= 0) return false;
         if (!this.hash) return false;
+        if (!this.data) return false;
+        if (this.timestamp < 1) return false;
+        if (!this.previousHash) return false;
         return true;
     }
 }
