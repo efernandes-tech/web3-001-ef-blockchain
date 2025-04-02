772aeb9d7cd134d666f736a053cd474f1d78a943
Éderson Fernandes <efernandes.tech@gmail.com>
2025-04-01 22:39:29 -0300

    Add Jest for unit tests

---
+++ __tests__/block.test.ts
@@ -0,0 +1,27 @@
+import {describe, test, expect} from '@jest/globals'
+import Block from '../src/lib/block'
+
+describe('Block tests', () => {
+
+    test('Should be valid', () => {
+        const block = new Block(1, 'abc')
+        const valid = block.isValid();
+
+        expect(valid).toBeTruthy();
+    })
+
+    test('Should NOT be valid (index)', () => {
+        const block = new Block(-1, 'abc')
+        const valid = block.isValid();
+
+        expect(valid).toBeFalsy();
+    })
+
+    test('Should NOT be valid (hash)', () => {
+        const block = new Block(1, '')
+        const valid = block.isValid();
+
+        expect(valid).toBeFalsy();
+    })
+
+})
---
+++ src/lib/blockchain.ts
@@ -0,0 +1,5 @@
+import Block from "./block";
+
+const block1 = new Block(1, "abcde");
+
+console.log(block1);
